a00ffca882d431080980f3737afe878424c1d425
matheusFellipeSales <matheusprobr@gmail.com>
2024-11-10 10:37:22 -0300

    padronizando as configs da waybar

--- waybar/.config/waybar/scripts/vpn_toggle.sh
+++ waybar/.config/waybar/scripts/vpn_toggle.sh
@@ -10,23 +10,23 @@ if [ "$1" = "toggle" ]; then
     if [ -n "$VPN_STATUS" ]; then
         # VPN está ativa, desconecta
         if nmcli connection down id "$VPN_NAME" >/dev/null 2>&1; then
-            echo '{ "text": "Hexa OFF", "class": "vpn-off" }'
+            echo '{ "text": "off", "class": "vpn-off" }'
         else
-            echo '{ "text": "VPN-Error", "class": "vpn-off" }'
+            echo '{ "text": "error", "class": "vpn-off" }'
         fi
     else
         # VPN não está ativa, conecta
         if nmcli connection up id "$VPN_NAME" >/dev/null 2>&1; then
-            echo '{ "text": "Hexa-ON", "class": "vpn-on" }'
+            echo '{ "text": "on", "class": "vpn-on" }'
         else
-            echo '{ "text": "VPN-Error", "class": "vpn-off" }'
+            echo '{ "text": "error", "class": "vpn-off" }'
         fi
     fi
 else
     if [ -n "$VPN_STATUS" ]; then
-        echo '{ "text": "Hexa ON", "class": "vpn-on" }'
+        echo '{ "text": "on", "class": "vpn-on" }'
     else
-        echo '{ "text": "Hexa OFF", "class": "vpn-off" }'
+        echo '{ "text": "off", "class": "vpn-off" }'
     fi
 fi
 
