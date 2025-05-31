8a170fe76a6ae5d4ca857217cd868f44f0cf6dbe
matheusFellipeSales <matheusprobr@gmail.com>
2025-05-31 12:11:33 -0300

    lazy: icons support for nestjs and test icons

---
+++ lazy/.config/nvim/lua/plugins/mini-icons.lua
@@ -0,0 +1,19 @@
+return {
+  {
+    "echasnovski/mini.icons",
+    opts = {
+      extension = {
+        -- Test icons
+        ["spec.ts"] = { glyph = "󰙨", hl = "MiniIconsOrange" },
+        ["e2e-spec.ts"] = { glyph = "󰙨", hl = "MiniIconsOrange" },
+        ["gateway-spec.ts"] = { glyph = "󰙨", hl = "MiniIconsOrange" },
+
+        -- Nest js
+        ["module.ts"] = { glyph = "", hl = "MiniIconsRed" },
+        ["service.ts"] = { glyph = "", hl = "MiniIconsYellow" },
+        ["controller.ts"] = { glyph = "", hl = "MiniIconsBlue" },
+        ["guard.ts"] = { glyph = "", hl = "MiniIconsGreen" },
+      },
+    },
+  },
+}
