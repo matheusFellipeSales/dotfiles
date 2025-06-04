12fa9b078e8a8f5029c4f49d070509d674015e71
matheusFellipeSales <matheusprobr@gmail.com>
2025-06-04 09:33:56 -0300

    lazy: add keymaps

--- lazy/.config/nvim/lua/config/keymaps.lua
+++ lazy/.config/nvim/lua/config/keymaps.lua
@@ -1,4 +1,8 @@
 -- Keymaps are automatically loaded on the VeryLazy event
 -- Default keymaps that are always set: https://github.com/LazyVim/LazyVim/blob/main/lua/lazyvim/config/keymaps.lua
 -- Add any additional keymaps here
-vim.opt.clipboard = "unnamedplus"
+
+vim.keymap.set("n", "<C-d>", "<C-d>zz", { desc = "Scroll down" })
+vim.keymap.set("n", "<C-u>", "<C-u>zz", { desc = "Scroll up" })
+vim.keymap.set("n", "n", "nzzzv", { desc = "Center next search result" })
+vim.keymap.set("n", "N", "Nzzzv", { desc = "Center previous search result" })
