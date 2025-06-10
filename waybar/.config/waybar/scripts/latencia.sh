bdb9d90d4774a54d44ca1251d8e90584d9915060
matheusFellipeSales <matheusprobr@gmail.com>
2025-06-10 08:27:29 -0300

    hypr updates

--- waybar/.config/waybar/scripts/latencia.sh
+++ waybar/.config/waybar/scripts/latencia.sh
@@ -8,12 +8,13 @@ LATENCIA=$(ping -c 1 -W 1 $DESTINO | grep 'tempo=' | awk -F'tempo=' '{print $2}'
 
 # Verifica se o ping foi bem-sucedido e formata o resultado em JSON
 if [ -n "$LATENCIA" ]; then
-    LATENCIA_INT=$(echo "$LATENCIA" | awk '{print int($1+0.5)}')
-    if (( $(echo "$LATENCIA > 70" | bc -l) )); then
-        echo "{\"text\": \"${LATENCIA} ms\", \"class\": \"warning\"}"
-    else
-        echo "{\"text\": \"${LATENCIA} ms\", \"class\": \"normal\"}"
-    fi
+  LATENCIA_INT=$(echo "$LATENCIA" | awk '{print int($1+0.5)}')
+  if (($(echo "$LATENCIA > 70" | bc -l))); then
+    echo "{\"text\": \"${LATENCIA} ms\", \"class\": \"warning\"}"
+  else
+    echo "{\"text\": \"${LATENCIA} ms\", \"class\": \"normal\"}"
+  fi
 else
-    echo "{\"text\": \"desconectado\", \"class\": \"disconnected\"}"
+  echo "{\"text\": \"desconectado\", \"class\": \"disconnected\"}"
 fi
+
--- waybar/.config/waybar/scripts/vpn_toggle.sh
+++ waybar/.config/waybar/scripts/vpn_toggle.sh
@@ -7,26 +7,25 @@ VPN_NAME="Hexanetworks"
 VPN_STATUS=$(nmcli -t -f NAME,TYPE connection show --active | grep "^$VPN_NAME:vpn$")
 
 if [ "$1" = "toggle" ]; then
-    if [ -n "$VPN_STATUS" ]; then
-        # VPN está ativa, desconecta
-        if nmcli connection down id "$VPN_NAME" >/dev/null 2>&1; then
-            echo '{ "text": "off", "class": "vpn-off" }'
-        else
-            echo '{ "text": "error", "class": "vpn-off" }'
-        fi
+  if [ -n "$VPN_STATUS" ]; then
+    # VPN está ativa, desconecta
+    if nmcli connection down id "$VPN_NAME" >/dev/null 2>&1; then
+      echo '{ "text": "off", "class": "vpn-off" }'
     else
-        # VPN não está ativa, conecta
-        if nmcli connection up id "$VPN_NAME" >/dev/null 2>&1; then
-            echo '{ "text": "on", "class": "vpn-on" }'
-        else
-            echo '{ "text": "error", "class": "vpn-off" }'
-        fi
+      echo '{ "text": "error", "class": "vpn-off" }'
     fi
+  else
+    # VPN não está ativa, conecta
+    if nmcli connection up id "$VPN_NAME" >/dev/null 2>&1; then
+      echo '{ "text": "on", "class": "vpn-on" }'
+    else
+      echo '{ "text": "error", "class": "vpn-off" }'
+    fi
+  fi
 else
-    if [ -n "$VPN_STATUS" ]; then
-        echo '{ "text": "on", "class": "vpn-on" }'
-    else
-        echo '{ "text": "off", "class": "vpn-off" }'
-    fi
+  if [ -n "$VPN_STATUS" ]; then
+    echo '{ "text": "on", "class": "vpn-on" }'
+  else
+    echo '{ "text": "off", "class": "vpn-off" }'
+  fi
 fi
-
