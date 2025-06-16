1375840aeb0e71f566cbb33688483770118cccd0
matheusFellipeSales <matheusprobr@gmail.com>
2025-06-16 12:49:45 -0300

    my-custom-config

---
+++ nvim/.config/nvim/lua/plugins/misc.lua
@@ -0,0 +1,12 @@
+return {
+  -- auto tag changer
+  {
+    "windwp/nvim-ts-autotag",
+    opts = {},
+  },
+  -- fidget
+  {
+    "j-hui/fidget.nvim",
+    opts = {},
+  },
+}
---
+++ nvim/.config/nvim/lua/plugins/snacks.lua
@@ -0,0 +1,19 @@
+return {
+  {
+    "folke/snacks.nvim",
+    priority = 1000,
+    lazy = false,
+    opts = {
+      animate = { enable = true },
+      bigfile = { enabled = true },
+      bufdelete = { enabled = true },
+      lazygit = { enabled = true },
+      indent = { enabled = true },
+      input = { enabled = true },
+      notifier = { enabled = true },
+      scope = { enabled = true },
+      scroll = { enabled = true },
+      statuscolumn = { enabled = true },
+    },
+  }
+}
