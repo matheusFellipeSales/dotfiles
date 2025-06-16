e22e569f22b24c149af645a6bc36e070ec5e310d
matheusFellipeSales <matheusprobr@gmail.com>
2025-06-16 14:31:03 -0300

    lala

--- lazy/.config/nvim/lua/config/keymaps.lua
+++ lazy/.config/nvim/lua/config/keymaps.lua
@@ -2,21 +2,23 @@
 -- Default keymaps that are always set: https://github.com/LazyVim/LazyVim/blob/main/lua/lazyvim/config/keymaps.lua
 -- Add any additional keymaps here
 
+local opts = { noremap = true, silent = true }
+
 -- centralize search results
-vim.keymap.set("n", "<C-d>", "<C-d>zz", { desc = "Scroll down" })
-vim.keymap.set("n", "<C-u>", "<C-u>zz", { desc = "Scroll up" })
-vim.keymap.set("n", "n", "nzzzv", { desc = "Center next search result" })
-vim.keymap.set("n", "N", "Nzzzv", { desc = "Center previous search result" })
+vim.keymap.set("n", "<C-d>", "<C-d>zz", opts)
+vim.keymap.set("n", "<C-u>", "<C-u>zz", opts)
+vim.keymap.set("n", "n", "nzzzv", opts)
+vim.keymap.set("n", "N", "Nzzzv", opts)
 
 -- better scape
-vim.keymap.set("i", "jj", "<Esc>", { noremap = false })
-vim.keymap.set("i", "jk", "<Esc>", { noremap = false })
+vim.keymap.set("i", "jj", "<Esc>", opts)
+vim.keymap.set("i", "jk", "<Esc>", opts)
 
 -- Paste without replacing
-vim.keymap.set("n", "<leader>p", '"_dP', { noremap = true, silent = true })
-vim.keymap.set("n", "x", '"_x', { noremap = true, silent = true })
+vim.keymap.set("v", "p", '"_dP', opts)
+vim.keymap.set("n", "x", '"_x', opts)
 
 -- Delete all buffers
 vim.keymap.set("n", "<leader>ba", function()
   Snacks.bufdelete.all()
-end, { desc = "Delete Other Buffers" })
+end, opts)
--- nvim/.config/nvim/lua/plugins/mason.lua
+++ nvim/.config/nvim/lua/plugins/mason.lua
@@ -1,29 +1,39 @@
 return {
-  {
-    "mason-org/mason-lspconfig.nvim",
-    opts = {
-      ensure_installed = { "lua_ls", "tsserver", "pyright", "eslint" },
-      servers = {
-        lua_ls = {
-          settings = {
-            Lua = {
-              diagnostics = {
-                globals = { "vim" },
-              },
-            },
-          },
-        },
-      },
-    },
-    dependencies = {
-      { "mason-org/mason.nvim", opts = {} },
-      "neovim/nvim-lspconfig",
-    },
-    config = function(_, opts)
-      for server, config in pairs(opts.servers) do
-        vim.lsp.config(server, config)
-        vim.lsp.enable(server)
-      end
-    end
-  },
+	{
+		"mason-org/mason-lspconfig.nvim",
+		opts = {
+			ensure_installed = { "lua_ls", "tsserver", "pyright", "eslint" },
+			servers = {
+				lua_ls = {
+					settings = {
+						Lua = {
+							diagnostics = {
+								globals = { "vim" },
+							},
+						},
+					},
+				},
+			},
+		},
+		dependencies = {
+			{ "mason-org/mason.nvim", opts = {} },
+			{
+				"neovim/nvim-lspconfig",
+				opts = {
+					tsserver = {
+						enabled = false,
+					},
+					ts_ls = {
+						enabled = false,
+					},
+				},
+			},
+		},
+		config = function(_, opts)
+			for server, config in pairs(opts.servers) do
+				vim.lsp.config(server, config)
+				vim.lsp.enable(server)
+			end
+		end,
+	},
 }
