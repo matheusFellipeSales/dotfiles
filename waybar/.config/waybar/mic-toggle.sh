60aa563c2fef3c1e86564b6139d3203dd9158926
mfellipeso <matheusprobr@gmail.com>
2026-04-10 15:17:12 -0300

    improve mic toggle script

--- waybar/.config/waybar/mic-toggle.sh
+++ waybar/.config/waybar/mic-toggle.sh
@@ -1,10 +1,4 @@
 #!/bin/bash
 pamixer --default-source -t
+swayosd-client --input-volume mute-toggle
 pkill -RTMIN+11 waybar
-
-if [ "$(pamixer --default-source --get-mute)" = "true" ]; then
-    notify-send -h string:x-canonical-private-synchronous:mic-toggle "Microfone" "Mutado 󰍭" --icon=microphone-sensitivity-muted
-else
-    VOLUME=$(pamixer --default-source --get-volume)
-    notify-send -h string:x-canonical-private-synchronous:mic-toggle "Microfone" "Ativo ${VOLUME}% 󰍬" --icon=microphone-sensitivity-high
-fi
