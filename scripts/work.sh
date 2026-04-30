ef6561e6fde8ba072c70c3c5286d0ae665a32f85
mfellipeso <matheusprobr@gmail.com>
2026-04-30 07:29:47 -0300

    put folder name

--- scripts/work.sh
+++ scripts/work.sh
@@ -5,6 +5,7 @@ work() {
   if [[ -n "$TMUX" ]]; then
     local nvim_pane claude_pane
     nvim_pane=$(tmux display-message -p '#{pane_id}')
+    tmux rename-window "$(basename "$PWD")"
     tmux send-keys -t "$nvim_pane" 'clear; exec nvim' C-m
     claude_pane=$(tmux split-window -h -c "$PWD" -l 30% -t "$nvim_pane" -P -F '#{pane_id}' 'zsh -ic claude')
     tmux split-window -v -c "$PWD" -l 20% -t "$nvim_pane"
@@ -17,7 +18,7 @@ work() {
     return
   fi
 
-  tmux new-session -d -s "$session_name" -c "$PWD" -x "$(tput cols)" -y "$(tput lines)" 'nvim'
+  tmux new-session -d -s "$session_name" -n "$(basename "$PWD")" -c "$PWD" -x "$(tput cols)" -y "$(tput lines)" 'nvim'
   local nvim_pane claude_pane
   nvim_pane=$(tmux display-message -t "$session_name":1 -p '#{pane_id}')
   claude_pane=$(tmux split-window -h -c "$PWD" -l 30% -t "$nvim_pane" -P -F '#{pane_id}' 'zsh -ic claude')
